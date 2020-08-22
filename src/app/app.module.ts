2e1511df6d593c242a409ee68b8668e53e9b36ad
Carlos <carlosmoretti2019@gmail.com>
2020-08-21 21:23:30 -0300

    corrigindo refresh no iis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CommonModule } from '@angular/common';
+import { CommonModule, LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { LoginModule } from './pages/login/login.module';
 import { AutenticadoComponent } from './pages/autenticado/autenticado.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -27,6 +27,7 @@ import { JwtRequestInterceptor } from './interceptors/jwt.interceptor';
     NgbModule
   ],
   providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
     { provide: HTTP_INTERCEPTORS, useClass: JwtRequestInterceptor, multi: true },
     JwtHelperService,
