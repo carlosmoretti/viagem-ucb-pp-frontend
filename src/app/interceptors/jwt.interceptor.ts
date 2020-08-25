453ebcf71464f1d1f3ad5e931a49454a455dcd96
Carlos <carlosmoretti2019@gmail.com>
2020-08-24 21:41:51 -0300

    retornando pro login caso token invalido

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,12 +1,15 @@
 import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginService } from '../service/login-service/login.service';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
     providedIn: "root"
 })
 export class JwtRequestInterceptor implements HttpInterceptor {
-    constructor(private auth: LoginService) {
+    constructor(private auth: LoginService, private router: Router) {
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler) {
@@ -16,6 +19,14 @@ export class JwtRequestInterceptor implements HttpInterceptor {
             }
         })
 
-        return next.handle(request);
+        return next.handle(request).pipe(
+            catchError((error: any) => {
+              if (error.status == 401 || error.status == 0) {
+                  localStorage.removeItem("token");
+                  this.router.navigate(['/login']);
+              }
+              return of(error);
+            })
+          );
     }
 }
